52f8f24223e30010032440eff04d15dd83b8df9b
Christian Haschek <christian@haschek.at>
2023-12-25 19:10:49 +0100

    don't kill the restart script

--- docker/rootfs/restart-dnsmasq.sh
+++ docker/rootfs/restart-dnsmasq.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 
 # if there is a running dnsmasq process, kill it and start a new one
-ps | grep dnsmasq | grep -v grep | awk '{print $1}' | xargs kill -9
+ps | grep dnsmasq | grep -v grep | grep -v 'restart' | awk '{print $1}' | xargs kill -9
 dnsmasq
