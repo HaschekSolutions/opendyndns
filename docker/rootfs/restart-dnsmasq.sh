bef7b33c757f497cf652132726ec131d7a28acec
Christian Haschek <christian@haschek.at>
2023-12-25 17:17:59 +0100

    smart restart

--- docker/rootfs/restart-dnsmasq.sh
+++ docker/rootfs/restart-dnsmasq.sh
@@ -1,3 +1,5 @@
 #!/bin/ash
 
-killall dnsmasq ; dnsmasq
+# if there is a running dnsmasq process, kill it and start a new one
+ps | grep dnsmasq | grep -v grep | awk '{print $1}' | xargs kill -9
+dnsmasq
